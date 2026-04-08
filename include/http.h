9508ac84552e4f27abb344a04ad13a66810a11fe
borshstudio <borshstudio@mail.ru>
2026-04-08 20:27:28 +0300

    Обновление стуктуры файлов и модулей

--- include/http.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by ASUS on 24.03.2026.
-//
-
-#ifndef _WX_HTTP_H
-#define _WX_HTTP_H
-
-#endif //_WX_HTTP_H
--- include/json_parser.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by ASUS on 24.03.2026.
-//
-
-#ifndef _WX_JSON_PARSER_H
-#define _WX_JSON_PARSER_H
-
-#endif //_WX_JSON_PARSER_H
