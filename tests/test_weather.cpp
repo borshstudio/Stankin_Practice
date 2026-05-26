3a0a459e31ccd4cbf8d88101f210d00e7a6bb43a
RomanDos331 <romandos.ru@gmail.com>
2026-05-26 21:18:03 +0300

    Add files via upload
    Добавлена папка tests с юнит-тестами, изменён CMakeLists.

---
+++ tests/test_weather.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include "weather.h"
+#include <vector>
+#include <string>
+
+std::string readUtf8Line() {
+    return "";
+}
+
+// Тест: Корректное сохранение и загрузка избранного
+TEST(WeatherLogicTest, FavoritesSaveAndLoad) {
+    std::vector<std::string> originalFavs = loadFavorites();
+
+    std::vector<std::string> testFavs = {"Москва", "Саратов", "Санкт-Петербург"};
+    
+    saveFavorites(testFavs);
+    std::vector<std::string> loadedFavs = loadFavorites();
+    
+    ASSERT_EQ(testFavs.size(), loadedFavs.size());
+    for (size_t i = 0; i < testFavs.size(); ++i) {
+        EXPECT_EQ(testFavs[i], loadedFavs[i]);
+    }
+
+    saveFavorites(originalFavs);
+}
+
+// Тесты: проверка правильного перевода городов
+TEST(WeatherLogicTest, CityTranslationDictionary) {
+    EXPECT_EQ(translateCityToEnglish("Москва"), "Moscow");
+}
+
+TEST(WeatherLogicTest, CityTransliteration) {
+    EXPECT_EQ(translateCityToEnglish("Тюмень"), "Tyumen");
+}
